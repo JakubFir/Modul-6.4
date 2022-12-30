fa2028bf12d8c4fe8b9befe69bd3680c9e69a460
Jakub <jakubfirlejczyk@gmail.com>
2022-12-31 00:46:24 +0100

    flights

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearch.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearch.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FlightSearch {
-    public boolean findFlight(Flight flight) throws RouteNotFoundException {
+    public String findFlight(Flight flight) throws RouteNotFoundException {
         Map<String, Boolean> flightMap = new HashMap<>();
         flightMap.put("asd", true);
         flightMap.put("dsa", false);
@@ -12,7 +12,7 @@ public class FlightSearch {
 
         if (flightMap.containsKey(flight.getDepartureAirport())) {
             System.out.println("flight exists");
-            return true;
+            return flight.getDepartureAirport();
         } else {
             throw new RouteNotFoundException();
         }
--- kodilla-exception/src/main/java/com/kodilla/exception/test/RoutNotFoundExceptionRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/RoutNotFoundExceptionRunner.java
@@ -3,9 +3,9 @@ package com.kodilla.exception.test;
 public class RoutNotFoundExceptionRunner {
     public static void main(String[] args) {
         FlightSearch flightSearch = new FlightSearch();
-        Flight asdd = new Flight("sda", "asd");
+        Flight asd = new Flight("asd", "asd");
         try {
-            flightSearch.findFlight(asdd);
+            flightSearch.findFlight(asd);
         } catch (RouteNotFoundException e) {
             System.out.println("flight doesnt exists");
         }
