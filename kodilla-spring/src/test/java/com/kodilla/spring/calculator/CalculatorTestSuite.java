adc760e9c497816e8a4fe133b3f4894cfbe85810
Jakub <jakubfirlejczyk@gmail.com>
2023-02-08 20:18:21 +0100

    test

--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -1,8 +1,14 @@
 package com.kodilla.spring.calculator;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class CalculatorTestSuite {
@@ -10,32 +16,39 @@ public class CalculatorTestSuite {
     private Calculator calculator;
 
     @Test
-    void testAdding(){
+    void testAdding() {
         //given
         //when
-        calculator.add(1,1);
+        double result = calculator.add(1, 1);
         //then
-    }
-    @Test
-    void testSub(){
-        //given
-        //when
-        calculator.sub(1,1);
-        //then
-    }
-    @Test
-    void testMul(){
-        //given
-        //when
-        calculator.mul(2,2);
-        //then
-    }
-    @Test
-    void testDiv(){
-        //given
-        //when
-        calculator.div(10,2);
-        //then
+        assertEquals(2, result);
     }
 
+    @Test
+    void testSub() {
+        //given
+        //when
+        double result = calculator.sub(1, 1);
+        //then
+        assertEquals(0, result);
+    }
+
+    @Test
+    void testMul() {
+        //given
+        //when
+        double result = calculator.mul(2, 2);
+        //then
+        assertEquals(4, result);
+    }
+
+    @Test
+    void testDiv() {
+        //given
+        //when
+        double result = calculator.div(10, 2);
+        //then
+        assertEquals(5, result);
+    }
 }
+
